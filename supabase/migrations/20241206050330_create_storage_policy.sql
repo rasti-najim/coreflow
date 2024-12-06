0d6134844258a24a31cdb5bef0681e238049605b
Rasti Aldawoodi <rastirasheed@gmail.com>
2024-12-06 00:06:45 -0500

    update

--- supabase/migrations/20241206050330_create_storage_policy.sql
+++
@@ -1,8 +0,0 @@
--- Create policy to allow authenticated users to view animations
-CREATE POLICY "Authenticated users can view animations"
-ON storage.objects FOR SELECT
-TO authenticated
-USING (
-  bucket_id = 'exercise_animations'
-  AND auth.role() = 'authenticated'
-)
