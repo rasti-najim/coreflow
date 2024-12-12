4f1fd5f85dc26e82cdc5a362b0628cf28f88291b
Rasti Aldawoodi <rastirasheed@gmail.com>
2024-12-11 21:06:21 -0500

    update

--- supabase/migrations/20241212011647_update_session_duration_enum.sql
+++ supabase/migrations/20241212011647_update_session_duration_enum.sql
@@ -1,16 +1,5 @@
--- Drop existing enum if it exists
-DROP TYPE IF EXISTS session_duration_enum CASCADE;
-
--- Create new enum type with all duration options
-CREATE TYPE session_duration_enum AS ENUM ('5', '10', '15', '20', '30');
-
--- Alter the user_preferences table to use the new enum type
-ALTER TABLE user_preferences 
-ALTER COLUMN session_duration TYPE session_duration_enum 
-USING session_duration::session_duration_enum;
-
--- Update existing values in the user_preferences table
-UPDATE user_preferences
-SET session_duration = '10'
-WHERE session_duration = '10-20';
+ALTER TYPE session_duration_enum ADD VALUE '10';
+ALTER TYPE session_duration_enum ADD VALUE '15';
+ALTER TYPE session_duration_enum ADD VALUE '20';
+ALTER TYPE session_duration_enum ADD VALUE '30';
 
