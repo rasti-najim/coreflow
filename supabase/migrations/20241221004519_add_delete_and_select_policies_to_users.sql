8ce3ce13770549a210dbc6ef2e433490324ebe69
Rasti Aldawoodi <rastirasheed@gmail.com>
2024-12-20 22:11:07 -0500

    fix migration

--- supabase/migrations/20241221004519_add_delete_and_select_policies_to_users.sql
+++ supabase/migrations/20241221004519_add_delete_and_select_policies_to_users.sql
@@ -1,11 +1,11 @@
-CREATE POLICY "Users can delete their own account" ON "users"
-AS PERMISSIVE
-FOR DELETE
-TO "users"
+CREATE POLICY "Users can view their own data" ON "users"
+FOR SELECT
 USING (auth.uid() = id);
 
-CREATE POLICY "Users can view their own data" ON "users"
-AS PERMISSIVE
-FOR SELECT
-TO "users"
+CREATE POLICY "Users can update their own data" ON "users"
+FOR UPDATE
+USING (auth.uid() = id);
+
+CREATE POLICY "Users can delete their own account" ON "users"
+FOR DELETE
 USING (auth.uid() = id);
