a7d6e3b41b47bc50430e23d01aeb89f4c87cfb7a
Rasti Aldawoodi <rastirasheed@gmail.com>
2025-01-13 16:37:48 -0500

    fix

--- supabase/migrations/20250113212634_update_weekly_sessions.sql
+++ supabase/migrations/20250113212634_update_weekly_sessions.sql
@@ -13,14 +13,26 @@ CREATE TYPE weekly_sessions_enum AS ENUM ('3', '5', 'everyday');
 ALTER TABLE user_preferences 
 ADD COLUMN weekly_sessions weekly_sessions_enum;
 
--- Restore the data with converted values
+-- Restore the data with converted values and handle nulls
 UPDATE user_preferences up
-SET weekly_sessions = 
-  CASE 
-    WHEN (SELECT weekly_sessions::text FROM temp_preferences tp WHERE tp.id = up.id) = '1-2' 
-    THEN '3'::weekly_sessions_enum
-    ELSE (SELECT weekly_sessions::text FROM temp_preferences tp WHERE tp.id = up.id)::weekly_sessions_enum
-  END;
+SET weekly_sessions = COALESCE(
+  (
+    CASE 
+      WHEN (SELECT weekly_sessions::text FROM temp_preferences tp WHERE tp.id = up.id) = '1-2' 
+      THEN '3'::weekly_sessions_enum
+      ELSE (SELECT weekly_sessions::text FROM temp_preferences tp WHERE tp.id = up.id)::weekly_sessions_enum
+    END
+  ),
+  '3'::weekly_sessions_enum  -- Default to '3' if null
+);
+
+-- Verify no null values exist
+DO $$
+BEGIN
+  IF EXISTS (SELECT 1 FROM user_preferences WHERE weekly_sessions IS NULL) THEN
+    RAISE EXCEPTION 'Null values still exist in weekly_sessions column';
+  END IF;
+END $$;
 
 -- Now make it NOT NULL after populating data
 ALTER TABLE user_preferences 
