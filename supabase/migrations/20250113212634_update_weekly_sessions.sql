ee2b89889f91a2e8f182d163fabeb8ec508389f1
Rasti Aldawoodi <rastirasheed@gmail.com>
2025-01-13 16:35:19 -0500

    fix

--- supabase/migrations/20250113212634_update_weekly_sessions.sql
+++ supabase/migrations/20250113212634_update_weekly_sessions.sql
@@ -1,15 +1,26 @@
--- Drop existing enum type if it exists
+-- First save existing data
+CREATE TABLE temp_preferences AS 
+SELECT * FROM user_preferences;
+
+-- Drop the existing enum type and table constraints
+ALTER TABLE user_preferences DROP COLUMN weekly_sessions;
 DROP TYPE IF EXISTS weekly_sessions_enum CASCADE;
 
 -- Create new enum type with updated values
 CREATE TYPE weekly_sessions_enum AS ENUM ('3', '5', 'everyday');
 
--- Alter the user_preferences table to use the new enum type
+-- Add the column back with the new type
 ALTER TABLE user_preferences 
-  ALTER COLUMN weekly_sessions TYPE weekly_sessions_enum 
-  USING (
-    CASE 
-      WHEN weekly_sessions::text = '1-2' THEN '3'::weekly_sessions_enum
-      ELSE weekly_sessions::text::weekly_sessions_enum
-    END
-  );
+ADD COLUMN weekly_sessions weekly_sessions_enum NOT NULL;
+
+-- Restore the data with converted values
+UPDATE user_preferences up
+SET weekly_sessions = 
+  CASE 
+    WHEN (SELECT weekly_sessions::text FROM temp_preferences tp WHERE tp.id = up.id) = '1-2' 
+    THEN '3'::weekly_sessions_enum
+    ELSE (SELECT weekly_sessions::text FROM temp_preferences tp WHERE tp.id = up.id)::weekly_sessions_enum
+  END;
+
+-- Clean up
+DROP TABLE temp_preferences;
